d3ba9a331f3660e385aa83513eb4c72e53e36ff5
Rafi 2cev Tucev <tucev@hotmail.com>
2024-05-30 10:42:51 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { getProducts } from "./services/productService.js";
+import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
+
+export default function App() {
+
+  const [products, setProducts] = useState([]);
+
+  // O_o => state from fetch
+  useEffect(() => {
+    getProducts("boards")
+      .then((response) => { setProducts(response);})
+  }, []);
+
+
+  function renderProduct(p) {
+    return (
+      <div key={p.id} className="product">
+        <a href="/">
+          <img src={`/images/${p.image}`} alt={p.name} />
+          <h3>{p.name}</h3>
+          <p>${p.price}</p>
+        </a>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <div className="content">
+        <Header />
+        <main>
+          <section id="filters">
+            <label htmlFor="size">Filter by Size:</label>{" "}
+            <select id="size">
+              <option value="">All sizes</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+            </select>
+          </section>
+        </main>
+      </div>
+      <Footer />
+    </>
+  );
+}
